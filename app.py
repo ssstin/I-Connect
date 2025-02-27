bd74f5ba74289b181a690ce3921a5b9974258cf0
Adrian <35592195+Black-Seal@users.noreply.github.com>
2025-02-27 13:26:17 +0800

    First version

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+def main():
+    st.title("Basic Streamlit App")
+    st.sidebar.header("User Input")
+
+    st.write(f"### Hello, World!")
+    
+if __name__ == "__main__":
+    main()
