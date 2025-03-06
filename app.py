79ae8f44ca6d3b638f13bcab68953668937ea258
Adrian <35592195+Black-Seal@users.noreply.github.com>
2025-03-06 23:42:29 +0800

    Version 1

--- app.py
+++ app.py
@@ -2,11 +2,23 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-def main():
-    st.title("Basic Streamlit App")
-    st.sidebar.header("User Input")
+from speechtospeech.can_to_eng import translate_cantonese_to_english_speech
+from speechtospeech.eng_to_can import translate_english_to_cantonese_speech
 
-    st.write(f"### Hello, World!")
-    
+def main():
+    st.title("Cantonese-English Speech Translator")
+
+    choice = st.radio(
+        "Choose an option:",
+        ("Speak in Cantonese and translate to English speech",
+         "Speak in English and translate to Cantonese speech")
+    )
+
+    if st.button("Translate"):
+        if choice == "Speak in Cantonese and translate to English speech":
+            translate_cantonese_to_english_speech()
+        elif choice == "Speak in English and translate to Cantonese speech":
+            translate_english_to_cantonese_speech()
+
 if __name__ == "__main__":
     main()
